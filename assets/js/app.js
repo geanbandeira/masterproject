d97359848fe20e7c7e5826aae7068d07fd24ff03
Gean Carlos de Sousa Bandeira <60990296+geanbandeira@users.noreply.github.com>
2024-10-11 16:48:51 -0300

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,6 +6,35 @@ const hamburger = document.querySelector('.menu-hamburger');
         navbar.classList.toggle('open');
     });
 
+var ctx = document.getElementById('kpiChart').getContext('2d');
+var kpiChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho'],
+        datasets: [{
+            label: 'Eficiência do Processo',
+            data: [85, 90, 88, 92, 95, 97],
+            backgroundColor: 'rgba(75, 192, 192, 0.2)',
+            borderColor: 'rgba(75, 192, 192, 1)',
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+const toggleButton = document.getElementById('dark-mode-toggle');
+const body = document.body;
+
+toggleButton.addEventListener('click', () => {
+    body.classList.toggle('dark-mode');
+});
+    
 //foquete, habilidade, comunidade
   document.addEventListener("DOMContentLoaded", () => {
     const blocks = document.querySelectorAll('.block');
@@ -204,3 +233,4 @@ document.addEventListener('DOMContentLoaded', () => {
       observer.observe(card);
   });
 });
+
